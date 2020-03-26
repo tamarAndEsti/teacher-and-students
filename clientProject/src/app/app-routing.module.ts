903ae0294324bcfdc6cfe755e4a19777a10aa61b
tamar <tamard2020@gmail.com>
2020-03-26 12:32:04 +0200

    sign in and sign up

--- clientProject/src/app/app-routing.module.ts
+++ clientProject/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LogInComponent } from './components/log-in/log-in.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LogInComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- clientProject/src/app/app.module.ts
+++ clientProject/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
